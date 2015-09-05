91e0ee4b01d15a38ca3f5172504e9be60b84520e
bernhard-hofmann <bhofmann@hotmail.co.uk>
2015-09-05 09:58:42 +0100

    First cut, copied from jsbin/lobozak.

---
+++ js/app.js
@@ -0,0 +1,89 @@
+// Private methods
+function showBytes() {
+	var byteArray = '';
+	var bitValue = [128,64,32,16,8,4,2,1];
+
+	for (var rowIx = 1; rowIx <= 8; rowIx++) {
+		var byteValue = 0;
+
+		for (var colIx = 1; colIx <= 8; colIx++) {
+			var id = '#'+ rowIx +'_'+ colIx;
+			var isChecked = $(id).hasClass('on');
+			if (isChecked) {
+				byteValue += bitValue[colIx-1];
+			}
+		}
+
+		if (rowIx > 1) {
+			byteArray += ',';
+		}
+
+		byteArray += '0x'+byteValue.toString(16);
+	}
+
+	$('#bytes').text(byteArray);
+}
+
+function setAll(checked) {
+	for (var rowIx = 1; rowIx <= 8; rowIx++) {
+		for (var colIx = 1; colIx <= 8; colIx++) {
+			var id = '#'+ rowIx +'_'+ colIx;
+			if (checked) {
+				$(id).addClass('on');
+			} else {
+				$(id).removeClass('on');
+			}
+		}
+	}
+
+	showBytes();
+}
+
+// Event handlers
+$('.led').click(function() {
+	if ($(this).hasClass('on')) {
+		$(this).removeClass('on');
+	} else {
+		$(this).addClass('on');
+	}
+	showBytes();
+});
+
+$('.bulkSelector').click(function() {
+  var id;
+	var $button = $(this);
+	var offset = $button.prop('id').substr(3);
+
+	for (var led=1; led<=8; led++) {
+  	if ($button.hasClass('column')) {
+			id = '#'+led+'_'+offset;
+		} else {
+			id = '#'+offset+'_'+led;
+		}
+		if ($button.hasClass('selected')){
+			$(id).removeClass('on');
+		} else {
+			$(id).addClass('on');
+		}
+	}
+
+	if ($button.hasClass('selected')){
+		$button.removeClass('selected');
+	} else {
+		$button.addClass('selected');
+	}
+
+	showBytes();
+});
+
+$('#clearAll').click(function(){
+	setAll(false);
+	$('.bulkSelector').removeClass('selected');
+});
+
+$('#setAll').click(function(){
+	setAll(true);
+});
+
+// Initialisation
+showBytes();
